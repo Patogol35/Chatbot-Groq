af0fe740e26cd2ee375ea23c2ed1eb7e3ef5a460
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-09-19 01:33:10 -0500

    Change reasoning_effort from low to medium

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -291,7 +291,7 @@ export const sendMessage = async (req, res) => {
             messages,
             temperature: 0.3,
             max_completion_tokens: MAX_COMPLETION_TOKENS,
-            reasoning_effort: "low",
+            reasoning_effort: "medium",
             stream: false,
         });
 
