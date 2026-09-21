5de815ca5e4ffa51081dddddc91a50704dbfc93c
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-09-20 20:30:41 -0500

    Update chat.controller.js

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -35,7 +35,7 @@ STACK:
 React, JavaScript, Django, Java, PostgreSQL, MySQL, Render, Vercel, AWS, VirtualBox, LibreOffice, Postman, RustDesk.
 
 PROYECTOS:
-Portfolio React, Quiz Ecuador, App del clima, Chatbot, Ajedrez y E-commerce React+Django.
+Portfolio React, Quiz Ecuador, App del clima, Chatbot, Ajedrez - StockFish y E-commerce React+Django.
 
 `;
 
